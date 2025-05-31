cb6f143420978ee6140e893b45f9ace87cc11e52
caitlinraymond12 <car950@miami.edu>
2025-05-30 21:13:41 -0400

    initial commit

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return ( 
+        <nav className="navbar">
+            <div className = "links">
+                <a href='/'>About</a>
+                <a href='/portfolio'>Portfolio</a>
+                <a href='contact'>Contact</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NavBar;
