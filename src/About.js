59de50cf980a6fe9718731fd1ef92f6d1457651c
caitlinraymond12 <car950@miami.edu>
2025-06-04 11:38:14 -0400

    portfolio

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className = "about">
             <div className = "info">
-                <h2>About</h2>
+                <h1>About</h1>
                 <p>I am a student at the University of Miami studying Computer Engineering. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repre.</p>
             </div>
             <div className = "headshot">
--- src/PortfolioItem.js
+++ src/PortfolioItem.js
@@ -7,9 +7,16 @@ const PortfolioItem = ({portfolios}) => {
         <div className ='portfolioItem'>
             { portfolios.map((portfolio) => (
                 <div className = 'portfolio-list' key={portfolio.id}>
-                    <h1>{portfolio.name}</h1>
-                    <p>{portfolio.description}</p> 
+                    <div class="container">
                         <img src={portfolio.img}/>
+                        <div class="middle">
+                            <div class="text">John Doe</div>
+                        </div>
+                    </div>
+                    <div className = "info">
+                        <h1>{portfolio.name}</h1>
+                        <p>{portfolio.description}</p> 
+                    </div>
                  </div>
             ))}
         </div>
@@ -17,3 +24,22 @@ const PortfolioItem = ({portfolios}) => {
 }
  
 export default PortfolioItem;
+
+
+
+/*
+
+    return ( 
+        <div className ='portfolioItem'>
+            { portfolios.map((portfolio) => (
+                <div className = 'portfolio-list' key={portfolio.id}>
+                    <img src={portfolio.img}/>
+                    <h1>{portfolio.name}</h1>
+                    <p>{portfolio.description}</p> 
+                 </div>
+            ))}
+        </div>
+     );
+}
+ 
+*/
