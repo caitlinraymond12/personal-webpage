7392270b69f72147f4d8b8483690acb1262ae2ae
caitlinraymond12 <car950@miami.edu>
2025-06-04 20:47:38 -0400

    starting contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,21 @@
 const Contact = () => {
     return ( 
         <div className = "contact">
-            <h1>Contact Me</h1>
-            <p>Github:caitlinraymond12</p>
-            <p>Linkedin: caitlinraymond12</p>
+            <h1>Let's Connect!</h1>
+            <div className = "reasons">
+                <div className = "one">
+                    <p>Interested in hearing more about my projects?</p>
+                    <h3>Contact Me</h3>
+                </div>
+                <div className = "two">
+                    <p>Have an idea for a new project?</p>
+                    <h3>Contact Me</h3>
+                </div>
+                <div className = "three">
+                    <p>Want to talk tech?</p>
+                    <h3>Contact Me</h3>
+                </div>
+            </div>
         </div>
      );
 }
