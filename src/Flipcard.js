d0f0e603e374e03d47308e9749562443f0cc6bfc
caitlinraymond12 <car950@miami.edu>
2025-06-05 23:17:39 -0400

    flipcard icons

--- src/Flipcard.js
+++ src/Flipcard.js
@@ -1,4 +1,5 @@
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 const Flipcard = () => {
     return ( 
@@ -8,8 +9,9 @@ const Flipcard = () => {
                                 <h3>Contact Me</h3>
                             </div>
                             <div class="flip-card-back">
-                                <a href="/" rel="noopener" target = '_blank'><FaGithub className="github-icon-2" / ></a>
-                                <a href="/" rel="noopener" target = '_blank'><FaLinkedin className="linkedin-icon-1" / ></a>
+                                <a href="https://github.com/caitlinraymond12" rel="noopener" target = '_blank'><FaGithub className="github-icon-2" / ></a>
+                                <a href="https://www.linkedin.com/in/caitlinaraymond/" rel="noopener" target = '_blank'><FaLinkedin className="linkedin-icon-1" / ></a>
+                                <a href="/" rel="noopener" target = '_blank'><MdEmail className="email-icon-1" / ></a>
                             </div>
                         </div>
         </div>
